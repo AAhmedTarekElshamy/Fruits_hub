e5a1a5de835751294c1c3041b53c2dcc4888333b
Ahmed Tarik <ahmedtarik20188@gmail.com>
2026-01-05 01:08:46 +0200

    Initial Flutter project

---
+++ lib/features/splash/views/widgets/splash_view_body.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../../gen/assets.gen.dart';
+import '../../../on_boarding/presentation/views/on_boarding_view.dart';
+
+class SplashViewBody extends StatefulWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  State<SplashViewBody> createState() => _SplashViewBodyState();
+}
+
+class _SplashViewBodyState extends State<SplashViewBody> {
+  @override
+  @override
+  void initState() {
+    super.initState();
+    executeNavigation();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [SvgPicture.asset(Assets.images.plant)],
+        ),
+        SvgPicture.asset(Assets.images.logo),
+        SvgPicture.asset(
+          Assets.images.splashBottom,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+
+
+  void executeNavigation() {
+    Future.delayed(
+        const Duration(seconds: 3),
+            () {
+          Navigator.pushReplacementNamed(context, OnBoardingView.routeName);
+        });
+  }
+/*
+*   void executeNavigation() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(
+          builder: (context) => const OnBoardingView(),
+        ),
+      );
+    });
+  }
+*/
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+import 'core/helper_functions/on_generate_routes.dart';
+import 'features/splash/views/splash_view.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+     debugShowCheckedModeBanner: false,
+      onGenerateRoute: onGenerateRoute,
+      initialRoute:SplashView.routeName ,
+    );
+  }
+}
+
